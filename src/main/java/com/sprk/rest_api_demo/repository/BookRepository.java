f342f8443f08d88989627cb6ff6e348634fe5df5
Gani25 <memonabdulgani3d@gmail.com>
2024-06-19 20:43:35 +0530

    Added JPA Repository and Deleted Hibernate

--- src/main/java/com/sprk/rest_api_demo/repository/BookRepository.java
+++ src/main/java/com/sprk/rest_api_demo/repository/BookRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.sprk.rest_api_demo.entity.Book;
 
+// Added to JPABranch
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
